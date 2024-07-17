ab06b15d49b1e05106eb1404c0239716592f04da
TetyanaLugova <lugovatanya1503@gmail.com>
2024-07-18 00:13:12 +0300

    HW3(finish)

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,6 +3,8 @@ import {
   getContactsController,
   getContactByIdController,
   createContactController,
+  patchContactController,
+  deleteContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -11,6 +13,11 @@ const router = Router();
 router.get('/contacts/', ctrlWrapper(getContactsController));
 
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
 router.post('/contacts', ctrlWrapper(createContactController));
+
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
