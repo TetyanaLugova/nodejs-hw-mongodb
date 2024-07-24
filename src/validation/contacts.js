83c69c2a4d1330b792f0517137949bb647f4208e
TetyanaLugova <lugovatanya1503@gmail.com>
2024-07-24 08:39:10 +0300

    hw4(2 fix)

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,13 +1,13 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required().message({
+  name: Joi.string().min(3).max(20).required().messages({
     'string.base': 'Username should be a string',
     'string.min': 'Username should have at least {#limit} characters',
     'string.max': 'Username should have at most {#limit} characters',
     'any.required': 'Username is required',
   }),
-  phoneNumber: Joi.string().min(3).max(20).required().message({
+  phoneNumber: Joi.string().min(3).max(20).required().messages({
     'string.base': 'Phone number should be, for example +38(ххх)ххх хх хх',
     'string.min': 'Phone number should have at least {#limit} characters',
     'string.max': 'Phone number should have at most {#limit} characters',
