abd8396ad6bfe5a28296140657eb2477d2fe6445
TetyanaLugova <lugovatanya1503@gmail.com>
2024-07-24 08:31:59 +0300

    hw4(2)

---
+++ src/validation/contacts.js
@@ -0,0 +1,31 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required().message({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).required().message({
+    'string.base': 'Phone number should be, for example +38(ххх)ххх хх хх',
+    'string.min': 'Phone number should have at least {#limit} characters',
+    'string.max': 'Phone number should have at most {#limit} characters',
+    'any.required': 'Phone number is required',
+  }),
+  email: Joi.string().min(3).max(20).email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string()
+    .min(3)
+    .max(20)
+    .valid('work', 'home', 'personal')
+    .required(),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().min(3).max(20).email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal'),
+});
