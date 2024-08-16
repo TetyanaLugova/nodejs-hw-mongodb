7f6078926b8d8b417be548ee4c8e4c40b49b213a
TetyanaLugova <lugovatanya1503@gmail.com>
2024-08-16 10:27:40 +0300

    HW7

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -44,4 +45,5 @@ export const setupServer = () => {
   });
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 };
