cba50958605761370dcbb66313d0d260d92cd4fa
TetyanaLugova <lugovatanya1503@gmail.com>
2024-08-16 11:08:22 +0300

    fix(HW7)

--- src/server.js
+++ src/server.js
@@ -36,6 +36,9 @@ export const setupServer = () => {
 
   app.use(router);
 
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
@@ -43,7 +46,4 @@ export const setupServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
 };
